2aa7aba3f2f1dd4479859c86f18a762d7a0e9d84
Christian Ceballos <christian.ceballos@bitsamericas.com>
2016-09-17 12:37:23 -0500

    Initial Commit

---
+++ Semana-2-Juego-de-Memoria/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Semana-2-Juego-de-Memoria
+//
+//  Created by Christian Ceballos on 9/17/16.
+//  Copyright © 2016 Christian Ceballos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
